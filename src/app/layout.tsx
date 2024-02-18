882ef243f9dea0a817c6e9ad58f9780ffde97c14
Egor <egorfrolov2019.ef@gmail.com>
2024-02-18 20:53:37 +0300

    chore: init

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { ColorSchemeScript, MantineProvider } from '@mantine/core';
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+import '@mantine/core/styles.css';
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -11,12 +11,17 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="ru">
+      <head>
+        <ColorSchemeScript />
+      </head>
+      <body>
+        <MantineProvider>{children}</MantineProvider>
+      </body>
     </html>
   );
 }
